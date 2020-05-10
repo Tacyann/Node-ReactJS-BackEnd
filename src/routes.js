f68ea1dbe40740b32ee8f46758b52bd9b2597a6f
Bernadete Lima Pimentel <TacyanneBernadete.Pimentel@cencosud.com.br>
2020-05-10 09:31:26 -0300

    Implementação Especialista

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 
 const EspecialistaController = require('./controllers/EspecialistaController');
-const MedicoController = require('./controllers/MedicoController');
-
+//const MedicoController = require('./controllers/MedicoController');
 
 /*desacoplando o modo de Rotas no express em uma nova variavel*/
 const routes = express.Router();
 
-routes.get('/medico', MedicoController.index);
-routes.post('/medico', MedicoController.create); 
+//routes.get('/medico', MedicoController.index);
+//routes.post('/medico', MedicoController.create); 
 routes.get('/especialidade', EspecialistaController.index);
 routes.post('/especialidade', EspecialistaController.create); 
 
