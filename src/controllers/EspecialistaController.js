e014f57a25df2869407f3a8fb833a00ab35f4763
Arthur Nunes <arthur.nunes@paygo.com.br>
2020-05-11 21:24:21 -0300

    comentários

--- src/controllers/EspecialistaController.js
+++ src/controllers/EspecialistaController.js
@@ -1,27 +1,62 @@
 const crypto = require('crypto');
 const connection = require('../database/connection');
 
-
-
 module.exports = {
 
-    async index(request, response){
-        const especialidade = await connection ('especialidade').select('*');
+    /** TODO:
+     *   - Quando formatar o código, coloque no máximo uma linha de espaçamento
+     *     entre uma coisa e outra.
+     *   - prefira separar o código nos moldes abaixo e utilizar um espaçamento 
+     *     entre eles (max 1 linha), como no exemplo da função "create" (linha 44)
+     *       > criação de variáveis/constantes
+     *       > lógica
+     *       > retorno
+     *   - usar mesma lógica acima para o arquivo js inteiro:
+     *       > requires
+     *       > exports
+     *       > criação de funções
+     *   - o module.exports pode ser utilizado como está hoje, ou como no exemplo abaixo:
+     *        ######################################
+     *        # ...inicio do arquivo               #
+     *        #                                    #
+     *        # const xx = require('xx')           #
+     *        #                                    #
+     *        # async minhaFuncao(a, b) {          #
+     *        #    ... minha implementação ...     #
+     *        # }                                  #
+     *        #                                    #
+     *        # module.exports = { minhaFuncao }   #
+     *        #                                    #
+     *        # ...fim do arquivo                  #
+     *        ######################################
+     */
 
+    async index(request, response) {
+        /**TODO: 
+         *  - tem que colocar um try catch que cubra todo esse código
+         *    pois se der erro no await, será retornado uma exceção.
+         *    O ideal é colocar o try/catch e retornar uma mensagem
+         *    tratada, tipo: "Não foi possível consultar as especialidades!"
+         */
+        const especialidade = await connection('especialidade').select('*');
         return response.json(especialidade);
     },
 
-    async create (request, response){
+    async create(request, response) {
+        /**TODO: 
+         *  - tem que colocar um try catch que cubra todo esse código
+         *    pois se der erro no await, será retornado uma exceção.
+         *    O ideal é colocar o try/catch e retornar uma mensagem
+         *    tratada, tipo: "Não foi possível cadastrar especialidade!"
+         */
         const { descEspecialidade } = request.body;
+        const idEspecialidade = crypto.randomBytes(4).toString('HEX');
 
-        const  idEspecialidade = crypto.randomBytes(4).toString('HEX');
-     
+        await connection('especialidade').insert({
+            idEspecialidade,
+            descEspecialidade,
+        })
 
-    await connection('especialidade').insert({
-       idEspecialidade,
-       descEspecialidade,
-    })   
-    
-        return response.json({idEspecialidade}); 
+        return response.json({ idEspecialidade });
     }
 };
--- src/controllers/MedicoController.js
+++ src/controllers/MedicoController.js
@@ -1,25 +1,38 @@
 const crypto = require('crypto');
 const connection = require('../database/connection');
 const EspecialistaController = require('./controllers/EspecialistaController');
-module.exports = {
-    async index (request, response){
-        
-        const medico = await connection('medico').select('*');
 
+module.exports = {
+
+    async index(request, response) {
+        /**TODO: 
+         *  - tem que colocar um try catch que cubra todo esse código
+         *    pois se der erro no await, será retornado uma exceção.
+         *    O ideal é colocar o try/catch e retornar uma mensagem
+         *    tratada, tipo: "Não foi possível consultar os médicos!"
+         */
+        const medico = await connection('medico').select('*');
         return response.json(medico);
     },
 
-    async create(request, response){
+    async create(request, response) {
+        /**TODO: 
+         *  - tem que colocar um try catch que cubra todo esse código
+         *    pois se der erro no await, será retornado uma exceção.
+         *    O ideal é colocar o try/catch e retornar uma mensagem
+         *    tratada, tipo: "Não foi possível cadastrar médico!"
+         *  - constante "nomeMedico" não existe (linha 31), a que deve ser 
+         *    usada é a "name" (linha 27)
+         */
+        const { name, CRM } = request.body;
+        const idEspecialidade = crypto.randomBytes(4).toString('HEX');
 
-    const { name, CRM } = request.body;  
-    const  idEspecialidade = crypto.randomBytes(4).toString('HEX');
+        await connection('medico').insert({
+            nomeMedico,
+            CRM,
+            idEspecialidade,
+        })
 
-    await connection('medico').insert({
-        nomeMedico,
-        CRM,
-        idEspecialidade,
-    })   
-    
-        return response.json({idEspecialidade}); 
+        return response.json({ idEspecialidade });
     }
 };
