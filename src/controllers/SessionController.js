42a63eb5d4f15fcc192b092a91e568d551d67118
Bernadete Lima Pimentel <TacyanneBernadete.Pimentel@cencosud.com.br>
2020-05-13 22:49:19 -0300

    SessionController

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,25 +1,23 @@
 const connection = require('../database/connection');
+const PacienteController = require('./PacienteController');
 
 module.exports = {
 
     async create(request, response) {
         try {
-            const { idPaciente } = request.body;
-
-            const paciente = await connection('paciente')
-                .where('idPaciente', idPaciente)
-                .select('this.nomePacientenomePaciente')
-                .first();
-
+            const paciente = await connection('paciente').select('idPaciente').first();
+            console.log(paciente);
             if (!paciente) {
                 return response.status(400).json({ error: 'Não foi encontrado Paciente com esse ID' });
             }
 
             return response.json(paciente);
+
         } catch (e) {
             console.log(e);
-            console.log("Não foi possível cadastrar médico!");
-            return response.json({ mensagem: "Não foi possível cadastrar médico!" })
+            console.log("Login não encontrado!");
+            return response.json({ mensagem: "Login não encontrado!" })
         }
     }
 }
+
