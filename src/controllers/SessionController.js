b0a60a0abfb16990245bf2bc5685eac18c05f955
Bernadete Lima Pimentel <TacyanneBernadete.Pimentel@cencosud.com.br>
2020-05-13 23:03:59 -0300

    SessionControler - Comentando o console.log

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -6,7 +6,7 @@ module.exports = {
     async create(request, response) {
         try {
             const paciente = await connection('paciente').select('idPaciente').first();
-            console.log(paciente);
+            //console.log(paciente);
             if (!paciente) {
                 return response.status(400).json({ error: 'Não foi encontrado Paciente com esse ID' });
             }
